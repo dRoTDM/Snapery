cc85cdc9ed068d71dc8e4c6cc7c5af7fb54ac502
dRoTDM <ironikxd@gmail.com>
2022-05-21 11:53:58 +0545

    constant

---
+++ lib/constants/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const TextStyle fieldTextStyle = TextStyle(color: Colors.white);
+
+const TextStyle fieldCalorieCalculatorHeader =
+    TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20);
+
+const TextStyle fieldBMIHeader =
+    TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18);
+const EdgeInsets fieldTextContentPadding = EdgeInsets.fromLTRB(20, 15, 20, 15);
