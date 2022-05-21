f05d3aa8a7ee6520ea21d49b5f903793b5bd6da3
dRoTDM <ironikxd@gmail.com>
2022-05-21 19:03:04 +0545

    bmi checked

---
+++ lib/screens/fooddiary/foodiary.dart
@@ -0,0 +1,13 @@
+class fooddiary extends StatefulWidget {
+  const fooddiary({Key? key}) : super(key: key);
+
+  @override
+  State<fooddiary> createState() => _fooddiaryState();
+}
+
+class _fooddiaryState extends State<fooddiary> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
