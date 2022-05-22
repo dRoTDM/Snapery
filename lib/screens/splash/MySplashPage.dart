3e06f7ee6dceed3a4301cb47d79b19446f5512b4
dRoTDM <ironikxd@gmail.com>
2022-05-22 16:19:31 +0545

    Splashscreen check

--- lib/screens/splash/MySplashPage.dart
+++ lib/screens/splash/MySplashPage.dart
@@ -1,29 +1,55 @@
-// ignore_for_file: prefer_const_constructors, unused_import, import_of_legacy_library_into_null_safe, file_names
+import 'package:snapery/config/app_navigation.dart';
 
+import '../constants/ic_config.dart';
 import 'package:flutter/material.dart';
-import 'package:snapery/homepage.dart';
-import 'package:snapery/main.dart';
-import 'package:splashscreen/splashscreen.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class MySplashPage extends StatefulWidget {
-  const MySplashPage({Key? key}) : super(key: key);
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key key}) : super(key: key);
 
   @override
-  _MySplashPageState createState() => _MySplashPageState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _MySplashPageState extends State<MySplashPage> {
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    navigateToRootHome(context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SplashScreen(
-      seconds: 10,
-      navigateAfterSeconds: HomePage,
-      imageBackground: Image.asset("assets/logo.jpg").image,
-      useLoader: true,
-      loaderColor: Colors.pink,
-      loadingText: Text(
-        "loading..",
-        style: TextStyle(color: Colors.grey),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 60,
+            ),
+            Text(
+              "Snapery.  \n Snap it , Snack it.",
+              style: TextStyle(
+                color: Colors.deepPurpleAccent,
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            SvgPicture.asset(
+              IconLogo,
+              color: Colors.deepPurpleAccent,
+              height: 400,
+              width: 400,
+            ),
+          ],
+        ),
       ),
     );
   }
