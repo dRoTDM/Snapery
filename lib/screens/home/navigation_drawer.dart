763798802d602d4b91b62f238b6a5eb7c8410534
dRoTDM <ironikxd@gmail.com>
2022-05-24 19:21:58 +0545

    navigation library plugin fix

--- lib/screens/home/navigation_drawer.dart
+++ lib/screens/home/navigation_drawer.dart
@@ -1,13 +1,161 @@
-class navigation extends StatefulWidget {
-  const navigation({Key? key}) : super(key: key);
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 
-  @override
-  State<navigation> createState() => _navigationState();
-}
+import '../../config/app_navigation.dart';
+import '../../data/bloc/homePage/home_page_bloc.dart';
+import '../../data/bloc/homePage/home_page_event.dart';
+import '../../screens/constants/ic_config.dart';
 
-class _navigationState extends State<navigation> {
+class NavigationDrawerParent extends StatelessWidget {
+  final User firebaseUser;
+  const NavigationDrawerParent({Key key, @required this.firebaseUser})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider(
+      create: (context) => HomePageBloc(),
+      child: NavigationDrawer(
+        firebaseUser: firebaseUser,
+      ),
+    );
+  }
+}
+
+class NavigationDrawer extends StatelessWidget {
+  final User firebaseUser;
+  NavigationDrawer({Key key, @required this.firebaseUser}) : super(key: key);
+  HomePageBloc homePageBloc;
+
+  @override
+  Widget build(BuildContext context) {
+    homePageBloc = BlocProvider.of<HomePageBloc>(context);
+    return Drawer(
+      child: Container(
+        color: Colors.deepPurpleAccent,
+        child: ListView(
+          children: <Widget>[
+            SvgPicture.asset(
+              IconLogo,
+              color: Colors.white,
+              height: 250,
+              width: 250,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(firebaseUser.photoURL ??
+                        "https://cdn3.iconfinder.com/data/icons/black-easy/512/538642-user_512x512.png"),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20),
+                        child: Text(
+                          "Signed in as: ",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 2),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20),
+                        child: Text(
+                          firebaseUser.email,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            GestureDetector(
+              onTap: () {
+                navigateToProfilePage(
+                  context: context,
+                  username: firebaseUser.email,
+                );
+              },
+              child: ListTile(
+                leading: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
+                title: Text("Food Diary",
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                navigateToBMIPage(
+                    context: context, username: firebaseUser.email);
+              },
+              child: ListTile(
+                leading: Icon(
+                  Icons.ballot_sharp,
+                  color: Colors.white,
+                ),
+                title: Text("BMI Calculator",
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                navigateToLibraryPage(context: context);
+              },
+              child: ListTile(
+                leading: Icon(
+                  Icons.ballot_sharp,
+                  color: Colors.white,
+                ),
+                title: Text("Libraries Used",
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+              ),
+            ),
+            Divider(
+              color: Colors.white70,
+            ),
+            GestureDetector(
+              onTap: () {
+                homePageBloc.add(
+                  LogoutButtonPressed(),
+                );
+              },
+              child: ListTile(
+                leading: Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ),
+                title: Text("Logout",
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/libraryused/library_list_view.dart
+++ lib/screens/libraryused/library_list_view.dart
@@ -1,13 +1,122 @@
-class list extends StatefulWidget {
-  const list({Key? key}) : super(key: key);
+import 'dart:convert';
 
-  @override
-  State<list> createState() => _listState();
-}
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' as rootBundle;
 
-class _listState extends State<list> {
+import '../../data/model/library_data_model.dart';
+
+class LibraryListView extends StatelessWidget {
+  const LibraryListView({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(left: 21.0, right: 22),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Libraries We Use",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 0.85,
+                child: const Text(
+                  "The following sets forth attribution notices for third party software that way be contained in portions of the Fitby product. We thank the open source community for all their contributions.",
+                  maxLines: 10,
+                  style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                      height: 1.5),
+                ),
+              ),
+            ],
+          ),
+          FutureBuilder(
+              future: readJsonData(),
+              builder: (context, data) {
+                if (data.hasError) {
+                  return const Center(
+                    child: Text('Error loading the json data'),
+                  );
+                } else if (data.hasData) {
+                  final items = data.data as List<LibraryDataModel>;
+                  return ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: items.isEmpty ? 0 : items.length,
+                    itemBuilder: (context, index) {
+                      return SizedBox(
+                        child: Column(
+                          children: <Widget>[
+                            ExpansionTile(
+                              title: Text(
+                                items[index].name.toString(),
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w300,
+                                    height: 1.5),
+                              ),
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(18.0),
+                                  child: Text(
+                                    items[index].license.toString(),
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w300,
+                                        height: 1.5),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    shrinkWrap: true,
+                  );
+                } else {
+                  return Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.red,
+                  );
+                }
+              }),
+        ],
+      ),
+    );
+  }
+
+  static Future<List<LibraryDataModel>> readJsonData() async {
+    final jsondata =
+        await rootBundle.rootBundle.loadString('assets/json/library.json');
+
+    final list = json.decode(jsondata) as List<dynamic>;
+
+    //map json and initialize using DataModel
+    return list
+        .map((e) => LibraryDataModel.fromJson(e as Map<String, dynamic>))
+        .toList();
   }
 }
